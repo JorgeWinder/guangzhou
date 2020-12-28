2635826f2698054b9dcf8bd009dd35c304585e47
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2020-12-28 18:20:09 -0500

    worked on Producto page using Api

---
+++ src/Api.js
@@ -0,0 +1,66 @@
+import fetch from 'node-fetch'; 
+
+async function getData(url){
+    const response = await fetch(url);
+    const data = await response.json();
+    return data
+}
+
+async function postData(url, form, header){
+
+    let options
+
+    options = {
+        method: "POST",
+        body: form,
+        headers: header
+    }
+
+    const response = await fetch(url,options)
+    return await response.json()
+
+}
+
+async function patchData(url, form, header){
+
+    let options
+
+    options = {
+        method: "PATCH",
+        body: form,
+        headers: header
+    }
+
+    const response = await fetch(url,options)
+
+    return await response.json()
+
+
+}
+
+
+async function deleteData(url, form, header){
+
+    let options
+
+    options = {
+        method: "DELETE",
+        body: form,
+        headers: header
+    }
+
+    const response = await fetch(url,options)
+
+    return await response.json()
+
+}
+
+
+const Api = {
+    getData,
+    postData,
+    patchData,
+    deleteData
+}
+
+export default Api
--- src/page/Producto.js
+++ src/page/Producto.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import img from '../../src/img/prod1.png'
 import marcas from '../../src/img/marcas.png'
 import ProductoRelacionado from '../components/ProductoRelacionado'
+import Api from '../Api'
 
 import { Link } from 'react-router-dom'
 
@@ -10,81 +11,91 @@ import './style/producto.css'
 
 export class Producto extends Component {
 
-    // state = {
-    //     currentIndex : 0
-    // }
+  state = {
+    producto: null,
+  }
 
-    componentDidMount() {
+    async componentDidMount() {
         window.scrollTo(0, 0);
+        const url = "http://localhost:3002/api-guangzhou-service/producto?codigo=1"
+        const data = await Api.getData(url)
+        this.setState({producto: data.body})
     }
 
-
-    render() {
+    render() {  
         return (
             <React.Fragment>
-
                 <section className="container" style={{'marginTop': '40px'}}>
-
                     <div className="row">
-
                         <div className="col l7 s12"> 
-
                             <Link to="/busqueda" style={{'display': 'flex', 'fontSize':'20px', 'alignItems':'center', 'color':'black'}}> 
                                 <i className="material-icons dp48">arrow_back</i>
                                 <span> Búsqueda por categoría y marca</span>
                             </Link>
-
                         </div>
-
-                    </div>
-                    
+                    </div>                  
                     <div className="row" style={{'marginTop': '-50px'}}>
                         <div className="col l6 s12">
-
                         </div>
                         <div className="col l6 s12 left">
                             <h1 className="titulo-producto">
-                                Anillos std
+                                {this.state.producto?.nombre}
                             </h1>
-
                             <div className="linea-producto"></div>
                         </div>
-
                     </div>
-
                     <div className="row">
                         <div className="col l6 s12 center">
                             <div className="marco-img-producto">
-                                <img className="responsive-img" src={img} alt=""/>
+                                <img className="responsive-img" src={this.state.producto?.imagen} alt=""/>
                             </div>
                         </div>
-
                         <div className="col l6 s12">
-
                             <p>
-                            En esta ocasión conocerás 10 Geniales inventos muy útiles para tu coche. Accesorios para tu auto realmente prácticos que te gustará conocer.
+                                {this.state.producto?.descripcion }
                             </p>
                             <p>
                                 <span className="texto-resaltado">Categoria de producto: </span>
-                                <span>Radamientos</span>
+                                <span>{this.state.producto?.categoria.nombre}</span>
                             </p>
                             <p>
                                 <span className="texto-resaltado">Repuesto para marcas: </span>
                             </p>
-
+                            <div className="row">
+                                {
+                                    this.state.producto?.marca.map((m) => {
+                                        return(
+                                            <div className="col">{m.nombre}</div>
+                                        )
+                                    })
+                                }
+                            </div>
                             <p>
                                 <img className="responsive-img" src={marcas} alt=""/>
                             </p>
-
                             <a href="#!" className="btn btn-large" style={{width: "100%", backgroundColor: "#5CCB67"}}>Hacer pedido via Whatsapp</a>
-
                         </div>
                     </div>
-
                 </section>
-
-                <ProductoRelacionado/>
-
+                <section className="container">
+                    <div className="row">
+                        <div className="col l6 s12 left">
+                            <h1 className="titulo-producto">
+                                Productos relacionados
+                        </h1>
+                            <div className="linea-producto"></div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        {
+                            this.state.producto?.productoRelacionado.map((producto) => {
+                                return(
+                                <ProductoRelacionado name={producto.nombre}/>
+                                )
+                            })
+                        }
+                    </div>
+                </section>
             </React.Fragment>
         )
     }
